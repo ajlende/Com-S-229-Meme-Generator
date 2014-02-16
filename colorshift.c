3a22de56ee75bf121bb32940898d7366ff279ae5
ajlende <ajlende@iastate.edu>
2014-02-16 14:04:29 -0600

    added more skeleton code for opening and closing the file

--- colorshift.c
+++ colorshift.c
@@ -6,6 +6,7 @@ int main(int argc, char** argv) {
 
 	FILE* infile;
 	FILE* outfile;
+	simp* simp_file;
 
 	infile = fopen( argv[1], "rb" );
 
@@ -22,9 +23,21 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
+	simp_file = (simp*) malloc(sizeof(simp));
+
+	readSimp(simp_file, infile);
+
+	/* Edit the photo here */
+
+	writeSimp(simp_file, outfile);
+
+	freeSimp(simp_file);
+
+	free(simp_file);
+
 	fclose(infile);
 	fclose(outfile);
-
 	
 	return 0;
 }
+
--- crop.c
+++ crop.c
@@ -6,6 +6,7 @@ int main(int argc, char** argv) {
 
 	FILE* infile;
 	FILE* outfile;
+	simp* simp_file;
 
 	infile = fopen( argv[1], "rb" );
 
@@ -21,6 +22,18 @@ int main(int argc, char** argv) {
 		fclose(infile);
 		return 1;
 	}
+
+	simp_file = (simp*) malloc(sizeof(simp));
+
+	readSimp(simp_file, infile);
+
+	/* Edit the photo here */
+
+	writeSimp(simp_file, outfile);
+
+	freeSimp(simp_file);
+
+	free(simp_file);
 
 	fclose(infile);
 	fclose(outfile);
