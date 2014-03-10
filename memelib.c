fec9b7f9a9b6cbdd0467939fb2046ca464043881
ajlende <ajlende@iastate.edu>
2014-03-10 15:32:03 -0500

    NEW meme library for meme related functions

---
+++ memelib.c
@@ -0,0 +1,52 @@
+/* 
+ * memelib.c
+ */
+
+#include "simp.h"
+#include "memelib.h"
+
+void initMeme(meme *meme_data, char *name) {
+	meme_data->name = name;
+	meme_data->num_attr = 0;
+}
+
+void addAttribute(meme *meme_data, char *name, char *msg, int x, int y) {
+
+	attribute* tmp = 0;
+	
+	tmp = (attribute*) realloc(meme_data->attr, (meme_data->num_attr + 1) * sizeof(attribute));
+
+	/* If realloc was successful... */
+	if (tmp) {
+		/* Copy the pointer over now that it's safe. */
+		meme_data->attr = tmp;
+		
+		/* Increment the number of attributes. */
+		(meme_data->num_attr)++;
+		
+		/* Set the values of the attribute. */
+		meme_data->attr[meme_data->num_attr - 1].name = name;
+		meme_data->attr[meme_data->num_attr - 1].msg = msg;
+		meme_data->attr[meme_data->num_attr - 1].x = x;
+		meme_data->attr[meme_data->num_attr - 1].y = y;
+	}
+
+}
+
+void freeMeme(meme *meme_data) {
+	
+	free(meme_data->name);
+	meme_data->name = 0;
+	
+	for (i = 0; i < meme_data->num_attr; i++) {
+		free(meme_data->attr[i]);
+		meme_data->attr[i] = 0;
+	}
+
+	free(meme_data->attr);
+	meme_data->attr = 0;
+}
+
+size_t readMeme() {
+	return 0;
+}
--- memelib.h
+++ memelib.h
@@ -1,9 +1,9 @@
 /*
- * meme.h
+ * memelib.h
  */
 
-#ifndef _MEME_H_
-#define _MEME_H_
+#ifndef _MEMELIB_H_
+#define _MEMELIB_H_
 
 typedef struct attribute_s {
 	char* name;
@@ -14,10 +14,13 @@ typedef struct attribute_s {
 
 typedef struct meme_s {
 	char* name;
+	int num_attr;
 	attribute* attr;
 } meme;
 
-void initMeme(meme *meme_data);
+void initMeme(meme *meme_data, char *name);
+
+void addAttribute(meme *meme_data, char *name, char *msg, int x, int y);
 
 void freeMeme(meme *meme_data);
 
