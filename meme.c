db7ed06dde0e73776695a3580767771c86cb262f
ajlende <ajlende@iastate.edu>
2014-03-10 15:39:31 -0500

    Did some more stuff to fix make
    … one of these days…

--- meme.c
+++ meme.c
@@ -5,7 +5,7 @@
 #define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
-#include "meme.h"
+#include "memelib.h"
 #include "font.h"
 #include "simp.h"
 
@@ -13,8 +13,14 @@ int main (int argc, char** argv) {
 
 	FILE* meme_file;
 	FILE* action_file;
+	FILE* font_file;
+	FILE* outfile;
+
 	char* line;
 	size_t line_size = 0;
+
+	meme meme_data;
+	font font_data;
 
 
 	/* Check to make sure there are the proper number of argumnets. */
@@ -27,27 +33,57 @@ int main (int argc, char** argv) {
 	/* Open the files for reading. If one fails to open, then exit and return 1. */
 	meme_file = fopen( argv[1], "r" );
 
-	if ( meme_file == 0 ) {
+	if (meme_file == 0) {
 		printf("File %s failed to open!\n", argv[1]);
 		return 1;
 	}
 	
 	action_file = fopen( argv[2], "r" );
 
-	if ( action_file == 0 ) {
+	if (action_file == 0) {
 		printf("File %s failed to open!\n", argv[2]);
+		fclose(meme_file);
 		return 1;
 	}
 
 
 	/* Read through the act file */
 	while (getline(&line, &line_size, action_file) != -1) {
-		printf("String: %sline_size: %zu\n", line, line_size);
+		/* TODO: remove testing print statements */
+		printf("--> %s", line);
+
+		/* For each line, take action based on what it starts with */
+		if (strncmp(line, "OUTFILE", 7) == 0) {
+			/* Open the outfile for writing binary. */
+			/* TODO: find the out_name. */
+			outfile = fopen(out_name, "wb");
+		} else if (strncmp(line, "MEME", 4) == 0) {
+			/* Initialize the meme structure with the given name. */
+			initMeme(meme_data, value);
+		} else if (strncmp(line, "FONT", 4) == 0) {
+			/* Initialize the font structure with the given name. */
+			
+		} else {
+			/* If the meme structure already exists, add attributes. */
+			
+		}
 	}
 
-	/* Read thoough the mem file */
+	
+	if (outfile == 0) {
+
+		printf("File '%s' failed to open!\n", out_name);
+
+		fclose(meme_file);
+		fclose(action_file);
+
+		return 1;
+	}
+
+	/* Read through the mem file */
 	while (getline(&line, &line_size, meme_file) != -1) {
-		printf("String: %sline_size: %zu\n", line, line_size);
+		printf("--> %s", line);
+		/* Split at the ':' delimiter */
 	}
 
 	/* Read through the fsf file */
@@ -61,6 +97,10 @@ int main (int argc, char** argv) {
 
 	fclose(meme_file);
 	fclose(action_file);
+	fclose(font_file);
+	fclose(outfile);
 
 	return 0;
 }
+
+
